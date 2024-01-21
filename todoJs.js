e3ce9c1174b7f7618e4c5c9aad12efe4d75c9154
kaberege <kabgnestor@gmail.com>
2024-01-21 17:15:37 +0200

    select on click

--- todoJs.js
+++ todoJs.js
@@ -1,7 +1,7 @@
 const todoList = document.getElementById("todo-list");
 const listButton = document.getElementById("list-btn");
 const listOutput = document.getElementById("listed");
-const filterSelect = document.getElementById("my-select");
+const filterSelect = document.querySelector(".my-select");
 
 //event Listners
 listButton.addEventListener("click",addList);
@@ -9,14 +9,14 @@ listOutput.addEventListener("click",deleteCheck);
 filterSelect.addEventListener("click",mySelected);
 
 //functions
-
+listOutput.innerHTML = "";
 function addList(){
   if(todoList.value === ""){
     alert("Enter text");
   }else{
     //mydiv section
     const myDiv = document.createElement("div");
-    myDiv.classList.add("my-div");
+    myDiv.classList.add("mydiv");
     
     // creating a radio button
     const  myCheckbox = document.createElement("input");
@@ -37,47 +37,46 @@ function addList(){
     myButton.classList.add("my-button");
     myDiv.appendChild(myButton);
     listOutput.appendChild(myDiv);
-    todoList.value = "";
   }
+    todoList.value = "";
 }
 
 // delete and check function
   function deleteCheck(e){
     const myItem = e.target;
     if(myItem.classList.contains("my-checkbox")){
-       const myChecked = myItem.parentElement;
-       myChecked.classList.toggle("completed");
+       const  mydiv = myItem.parentElement;
+        mydiv.classList.toggle("completed");
     }
    
     if(myItem.classList[0] === "my-button"){
-      const myRemove = myItem.parentElement;
-      myRemove.remove();
+      const  mydiv = myItem.parentElement;
+      mydiv.remove();
     }
 }
 
 //select completed task function
-
 function mySelected(e){
-  const doList = listOutput.childNodes;
-  doList.forEach(function(myDiv){
+ const doList = listOutput.childNodes;
+ doList.forEach(function(mydiv){
     switch(e.target.value){
     case "all":
-       myDiv.style.display = "flex";
+       mydiv.style.display = "flex";
       break;
     case "completed":
-      if(myDiv.classList.contains("completed")){
-        myDiv.style.display = "flex";
+      if(mydiv.classList.contains("completed")){
+        mydiv.style.display = "flex";
       }else{
-         myDiv.style.display = "none";
+         mydiv.style.display = "none";
        } 
       break;
      case "uncompleted": 
-        if(!myDiv.classList.contains("completed")){
-        myDiv.style.display = "flex";
+        if(!mydiv.classList.contains("completed")){
+        mydiv.style.display = "flex";
       }else{
-         myDiv.style.display = "none";
+         mydiv.style.display = "none";
        } 
        break;   
 }
 });
-}
+} 
